f5169dabf689abfe8544ac6a64348a6bc1cf1ea8
silviacremascoli <silcremascoli@gmail.com>
2023-06-19 13:15:33 +0200

    Added moment.js and improved general code readability

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-function backgroundColor() {
+const backgroundColor = () => {
   let time = new Date().getHours();
   if (time > 6 && time < 20) {
     document.querySelector("body").classList.add("day");
@@ -6,46 +6,27 @@ function backgroundColor() {
   } else {
     document.querySelector("body").classList.add("night");
     document.querySelector("body").classList.add("night");
-    document.querySelector("#github-link").classList.add("night-text");
+    document.getElementById("github-link").classList.add("night-text");
     document.querySelector("footer").classList.add("night-text");
     document.querySelector("body").classList.remove("day");
   }
-}
+};
 
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let weekDays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let weekDay = weekDays[date.getDay()];
-  return `${weekDay}, ${hours}:${minutes}`;
-}
-
-function formatForecastDay(timestamp) {
+const formatDate = (timestamp) => {
   let date = new Date(timestamp * 1000);
-  let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let weekDay = weekDays[date.getDay()];
+  let formattedDate = moment(date).format("dddd, HH:mm");
+  return formattedDate;
+};
 
-  return weekDay;
-}
+const formatForecastDay = (timestamp) => {
+  let date = new Date(timestamp * 1000);
+  let formattedDate = moment(date).format("ddd");
+  return formattedDate;
+};
 
-function displayForecast(response) {
-  let dailyForecast = response.data.daily;
-  let forecast = document.querySelector("#forecast");
+const displayForecast = (response) => {
+  const dailyForecast = response.data.daily;
+  const forecast = document.getElementById("forecast");
 
   let forecastHTML = `<div class="row">`;
   dailyForecast.forEach(function (forecastDay, index) {
@@ -80,72 +61,66 @@ function displayForecast(response) {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecast.innerHTML = forecastHTML;
-}
+};
 
-function getForecast(coordinates) {
+const getForecast = (coordinates) => {
   let apiKey = "372b3246a78f090c2oeea103eb8344t0";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-}
+};
 
-function showTemperature(response) {
-  document.querySelector("#city").innerHTML = response.data.city;
-  document.querySelector("#temperature").innerHTML = Math.round(
+const showTemperature = (response) => {
+  document.getElementById("city").innerHTML = response.data.city;
+  document.getElementById("temperature").innerHTML = Math.round(
     response.data.temperature.current
   );
-  document.querySelector("#humidity").innerHTML =
+  document.getElementById("humidity").innerHTML =
     response.data.temperature.humidity;
-  document.querySelector("#wind-speed").innerHTML = Math.round(
+  document.getElementById("wind-speed").innerHTML = Math.round(
     response.data.wind.speed
   );
-  document.querySelector("#description").innerHTML =
+  document.getElementById("description").innerHTML =
     response.data.condition.description;
-  document.querySelector("#date").innerHTML = formatDate(
-    response.data.time * 1000
-  );
+  document.getElementById("date").innerHTML = formatDate(response.data.time);
   document
-    .querySelector("#icon")
+    .getElementById("icon")
     .setAttribute(
       "src",
       `https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
     );
   document
-    .querySelector("#icon")
+    .getElementById("icon")
     .setAttribute("alt", response.data.condition.description);
 
   getForecast(response.data.coordinates);
-}
+};
 
-function getCity(event) {
+const form = document.getElementById("search-form");
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  let cityInput = document.querySelector("#search-input");
+  let cityInput = document.getElementById("search-input");
   searchCity(cityInput.value);
-}
+});
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", getCity);
-
-function getMyPosition(position) {
+const getMyPosition = (position) => {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let apiKey = "372b3246a78f090c2oeea103eb8344t0";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
-}
+};
 
-function getPosition(event) {
+const locationButton = document.getElementById("my-location-button");
+locationButton.addEventListener("click", (event) => {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(getMyPosition);
-}
+});
 
-let locationButton = document.querySelector("#my-location-button");
-locationButton.addEventListener("click", getPosition);
-
-function searchCity(city) {
+const searchCity = (city) => {
   let apiKey = "372b3246a78f090c2oeea103eb8344t0";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
-}
+};
 
 searchCity("Milan");
 backgroundColor();
